3cebd2cdd0b0590d0841ac70bb4ec26f1980aaac
tdude0175 <46455427+tdude0175@users.noreply.github.com>
2019-10-07 10:46:09 -0500

    Initializing project online
    First attempt at a c# based project so pushing current work online to work on via multiple machienes.

---
+++ myApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        public static bool greetRun = true;
+        public static string user = "";
+
+	    static void Main(string[] args)
+	    {
+	        //WorkingWithIntegers();
+	        while(isRunning)
+            {
+                Console.WriteLine("Welcome what would you like to do? 1.say hello 2.Exit");
+                string isTheUserStaying = Console.ReadLine();
+                if(isTheUserStaying == "2" || isTheUserStaying == "exit")
+                {
+                    Console.WriteLine("GoodBye");
+                    isRunning = false;
+                }
+                if(isTheUserStaying == "1")
+                {
+                    Console.WriteLine("What is your name?");
+                    string user =  Console.ReadLine();
+                    Console.WriteLine($"Hello {user}");
+                    Console.WriteLine();
+                    greetRun = false;
+                }
+            }
+	    }
+        static void
+    }
+}
