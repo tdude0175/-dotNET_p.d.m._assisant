122b378072ef3da61c2e1a807b35204cff7644db
tdude0175 <tdude0175@gmail.com>
2019-10-10 16:21:02 -0500

    set proper model for use in testing sql database

--- myApp/BookModel.cs
+++ myApp/BookModel.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 
 
 namespace mysqlefcore
@@ -9,5 +10,13 @@ namespace mysqlefcore
         public string Aurthor { get; set;}
         public string Language { get; set; }
         public int Pages { get; set; }
+        public virtual Publisher Publisher { get; set; }
+    }
+
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
     }
 }
